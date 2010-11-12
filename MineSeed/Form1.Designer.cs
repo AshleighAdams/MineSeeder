d4b79d3f9490ecb7b48c2e7782b792da5198ec40
c0bra61 <C0BRA@.(none)>
2010-11-12 00:09:25 +0000

    Init

---
+++ MineSeed/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace MineSeed
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.tbCode = new System.Windows.Forms.TextBox();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "level.dat";
+            this.openFileDialog.Filter = "MineCraft Levels|level.dat";
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(213, 38);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(75, 23);
+            this.btnEncode.TabIndex = 0;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // tbCode
+            // 
+            this.tbCode.Location = new System.Drawing.Point(12, 12);
+            this.tbCode.Name = "tbCode";
+            this.tbCode.Size = new System.Drawing.Size(357, 20);
+            this.tbCode.TabIndex = 1;
+            this.tbCode.Text = "[Y3NT5/qeKVa1twJlZ2dsb+0gZWEzdGVy4pqYmKUalmSzDVU2]";
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(294, 38);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(75, 23);
+            this.btnDecode.TabIndex = 2;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 72);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.tbCode);
+            this.Controls.Add(this.btnEncode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "MineSeeder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.TextBox tbCode;
+        private System.Windows.Forms.Button btnDecode;
+    }
+}
+
---
+++ MineSeed/NBT/MineSeed.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using LibNbt;
+using LibNbt.Tags;
+
+namespace MineSeed
+{
+    class MineSeeder
+    {
+        
+
+        public static string Get(string world_file)
+        {
+            byte[] XOR_KEY = { 108, 111, 108, 32, 101, 97, 115, 116, 101, 114, 32, 101, 103, 103 };
+
+            NbtFile nbt = new NbtFile(world_file, true);
+            nbt.LoadFile();
+            
+            byte[] randSeed = BitConverter.GetBytes(nbt.RootTag.Tags[0].Query<NbtLong>("/Data/RandomSeed").Value);
+            byte[] time = BitConverter.GetBytes(nbt.RootTag.Tags[0].Query<NbtLong>("/Data/Time").Value);
+
+            byte[] playerX = BitConverter.GetBytes((int)nbt.RootTag.Tags[0].Query<NbtDouble>("/Data/Player/Pos/0").Value);
+            byte[] playerY = BitConverter.GetBytes((int)nbt.RootTag.Tags[0].Query<NbtDouble>("/Data/Player/Pos/1").Value);
+            byte[] playerZ = BitConverter.GetBytes((int)nbt.RootTag.Tags[0].Query<NbtDouble>("/Data/Player/Pos/2").Value);
+
+            byte[] rotationX = BitConverter.GetBytes(nbt.RootTag.Tags[0].Query<NbtFloat>("/Data/Player/Rotation/0").Value);
+            byte[] rotationY = BitConverter.GetBytes(nbt.RootTag.Tags[0].Query<NbtFloat>("/Data/Player/Rotation/1").Value);
+
+            uint size = (sizeof(long) * 2) + (sizeof(int) * 3) + (sizeof(float) * 2);
+
+            byte[] data = new byte[size];
+
+            int end = 0;
+            
+            Array.Copy(randSeed, 0, data, end, randSeed.Length);
+            end += randSeed.Length;
+            Array.Copy(time, 0, data, end, time.Length);
+            end += time.Length;
+            Array.Copy(playerX, 0, data, end, playerX.Length);
+            end += playerX.Length;
+            Array.Copy(playerY, 0, data, end, playerY.Length);
+            end += playerY.Length;
+            Array.Copy(playerZ, 0, data, end, playerZ.Length);
+            end += playerZ.Length;
+            Array.Copy(rotationX, 0, data, end, rotationX.Length);
+            end += rotationX.Length;
+            Array.Copy(rotationY, 0, data, end, rotationY.Length);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int mod = i % XOR_KEY.Length;
+                data[i] = (byte)((int)data[i] ^ (int)XOR_KEY[mod]);
+            }
+
+            return "[" + Convert.ToBase64String(data) + "]";
+        }
+
+        public static void Set(string input, string file)
+        {
+            byte[] XOR_KEY = { 108, 111, 108, 32, 101, 97, 115, 116, 101, 114, 32, 101, 103, 103 };
+
+            if (!input.StartsWith("[") || !input.EndsWith("]"))
+                return;
+            input = input.Replace("[", "").Replace("]", "");
+            byte[] data = Convert.FromBase64String(input);
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                int mod = i % XOR_KEY.Length;
+                data[i] = (byte)((int)data[i] ^ (int)XOR_KEY[mod]);
+            }
+
+            long randomSeed = BitConverter.ToInt64(data, 0);
+            long time = BitConverter.ToInt64(data, sizeof(long));
+
+        }
+
+    }
+}
